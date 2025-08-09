11af964b13798aebc11cf596507fd4fd2243b44e
Liu Wenyuan <15816141883@163.com>
2025-08-10 06:57:40 +0800

    Include relevant Java files in libcore for luci-io test

--- tests/core/luni-io/Android.mk
+++ tests/core/luni-io/Android.mk
@@ -28,6 +28,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under,../../../../libcore/luni/src/test
 	$(call all-java-files-under,../../../../libcore/support/src/test/java/) \
 	$(call all-java-files-under,../../../../libcore/luni/src/test/java/org/apache/harmony/luni/tests/pkg1) \
 	$(call all-java-files-under,../../../../libcore/luni/src/test/java/org/apache/harmony/luni/tests/pkg2) \
+	$(call all-java-files-under,../../../../libcore/luni/src/test/java/org/apache/harmony/luni/tests/java/io) \
 	../../../../libcore/luni/src/test/java/tests/luni/AllTestsIo.java
 
 LOCAL_PACKAGE_NAME := android.core.tests.luni.io
